18e822155b9fb40a760e56dd75903433a0036fa0
SantiagoToro2023 <santiago.toro@sidmar.ch>
2024-10-30 15:36:31 +0100

    Fixed indentation oopsies 30.10.2024 - ST

--- rock_paper_scissors_ST.py
+++ rock_paper_scissors_ST.py
@@ -1,6 +1,5 @@
-# Santiago Toro - M122 - 30.12.2024
-# Rock, Paper, Scissors - Grundlagen Python
 import random
+
 print('''
 |-----------------------------|
 |     Rock, Paper Scissor     |
@@ -8,40 +7,38 @@ print('''
 |-----------------------------|
 ''')
 
+WEAPONS = ['Rock', 'Paper', 'Scissor']
+
+def user_win():
+    print('System has chosen: ' + SYSTEM_WEAPON)
+    print('You win!')
+
+def user_lose():
+    print('System has chosen: ' + SYSTEM_WEAPON)
+    print('You lose!')
+
+def user_tie():
+    print('System has chosen: ' + SYSTEM_WEAPON)
+    print('It\'s a tie!')
+
+def determine_outcome(USER_WEAPON, SYSTEM_WEAPON):
+    if USER_WEAPON == SYSTEM_WEAPON:
+        user_tie()
+    elif (USER_WEAPON == 'Rock' and SYSTEM_WEAPON == 'Scissor') or \
+         (USER_WEAPON == 'Paper' and SYSTEM_WEAPON == 'Rock') or \
+         (USER_WEAPON == 'Scissor' and SYSTEM_WEAPON == 'Paper'):
+        user_win()
+    else:
+        user_lose()
+
+# Main game loop
 while True:
-    WEAPONS = ['Rock', 'Paper', 'Scissor']
-    USER_WEAPON = input('Choose your weapon! (Rock, Paper or Scissor): ')
+    USER_WEAPON = input('Choose your weapon! (Rock, Paper or Scissor): ').capitalize()
     SYSTEM_WEAPON = random.choice(WEAPONS)
 
-    def user_win():
-        print('System has chosen: ' + SYSTEM_WEAPON)
-        print('You win!')
-    def user_lose():
-        print('System has chosen: ' + SYSTEM_WEAPON)
-        print('You lose!')
-    def user_tie():
-        print('System has chosen: ' + SYSTEM_WEAPON)
-        print('Its a tie!')
-
-
-    def determine_outcome(USER_WEAPON, SYSTEM_WEAPON):
-        if USER_WEAPON == SYSTEM_WEAPON:
-            user_tie(SYSTEM_WEAPON)
-        elif (USER_WEAPON == 'Rock' and SYSTEM_WEAPON == 'Scissor') or \
-                (USER_WEAPON == 'Paper' and SYSTEM_WEAPON == 'Rock') or \
-                (USER_WEAPON == 'Scissor' and SYSTEM_WEAPON == 'Paper'):
-            user_win(SYSTEM_WEAPON)
-        else:
-            user_lose(SYSTEM_WEAPON)
-
-
-    while True:
-        USER_WEAPON = input('Choose your weapon! (Rock, Paper or Scissor): ').capitalize()
-        SYSTEM_WEAPON = random.choice(WEAPONS)
-
-        if USER_WEAPON in WEAPONS:
-            determine_outcome(USER_WEAPON, SYSTEM_WEAPON)
-        else:
-            print('Invalid input you incompetent fool! Go back to first grade! You suck! 🤬🤬🤬🤬🤬')
-            print('*ᵁⁿˡᵉˢˢ ʸᵒᵘʳ ˢᵃⁿᵗᶦᵃᵍᵒ ᵀᵒʳᵒ ʰᵉˢ ᵏᵉʷˡ 😎')
-            break
+    if USER_WEAPON in WEAPONS:
+        determine_outcome(USER_WEAPON, SYSTEM_WEAPON)
+    else:
+        print('Invalid input you incompetent fool! Go back to first grade! You suck! 🤬🤬🤬🤬🤬')
+        print('*ᵁⁿˡᵉˢˢ ʸᵒᵘʳ ˢᵃⁿᵗᶦᵃᵍᵒ ᵀᵒʳᵒ ʰᵉˢ ᵏᵉʷˡ 😎')
+        break
